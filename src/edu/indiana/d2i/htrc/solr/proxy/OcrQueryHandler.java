780763689e0dfd7b14dd1bb68c1b7b2e909df7a9
pengzong1111 <pengzong1111@gmail.com>
2014-09-19 16:38:49 -0400

    distinguish audit log from debug log

--- src/edu/indiana/d2i/htrc/solr/proxy/OcrQueryHandler.java
+++ src/edu/indiana/d2i/htrc/solr/proxy/OcrQueryHandler.java
@@ -54,126 +54,74 @@ import edu.indiana.d2i.htrc.solr.output.ErrorStreamingOutput;
 
 @Path("/ocr/select")
 public class OcrQueryHandler {
-	@Context ServletConfig servletConfig;
-	
-	private static final Logger logger = Logger
-			.getLogger(OcrQueryHandler.class.getName());
+	@Context
+	ServletConfig servletConfig;
+
+	private static final Logger auditLogger = Logger.getLogger("auditLogger");
+	private static final Logger debugLogger = Logger.getLogger("debugLogger");
 
 	/**
-	 * get query string from client, forward it to real solr instance and stream the result 
-	 * returned from solr instance back to client. 
+	 * get query string from client, forward it to real solr instance and stream
+	 * the result returned from solr instance back to client.
 	 * 
-	 * @param query_string recieved query string from client side.
-	 * @param ui UriInfo object got from Context in jersey framework.
-	 * @param hsr HttpServletRequest object got from Context in jersey framework.
-	 * @return a StreamingOutput object that streams content returned from solr instance back to client.
+	 * @param query_string
+	 *            recieved query string from client side.
+	 * @param ui
+	 *            UriInfo object got from Context in jersey framework.
+	 * @param hsr
+	 *            HttpServletRequest object got from Context in jersey
+	 *            framework.
+	 * @return a StreamingOutput object that streams content returned from solr
+	 *         instance back to client.
 	 * @throws IOException
 	 */
 	@GET
-	//@Produces(MediaType.TEXT_XML)
-	public Response getOcrQueryResult(
-			@Context UriInfo ui, @Context final HttpServletRequest hsr)
-			{
-		
+	public Response getOcrQueryResult(@Context UriInfo ui, @Context final HttpServletRequest hsr) {
 		final URI uri = ui.getRequestUri();
-		
 		UriBuilder uriBuilder = ui.getRequestUriBuilder();
-		
-		Set<String> queryParamKeySet = ui.getQueryParameters().keySet();
-		
-	//	System.out.println(queryParamKeySet);
 		ParamDefinition ParamDef = new ParamDefinition(servletConfig);
-		
+
 		String[] hosts = ParamDef.getConfig().getProperty("solr.ocr.host").split(",");
 		int hostNum = hosts.length;
 		String host = hosts[(int) (Math.random() * hostNum)];
-		System.out.println("dispatch to " + host);
-		URI newURI = uriBuilder.host(host)
-					.port(Integer.valueOf(ParamDef.getConfig()
-							.getProperty("solr.ocr.port")))
-							.replacePath("solr/" + ParamDef.getConfig()
-									.getProperty("solr.ocr.core")+"/select")
-							.build();  
-		
-		
-		System.out.println("new OCR uri: " + newURI);
-		
-		URL newURL = null;
-		try {
-			newURL = newURI.toURL();
-		} catch (MalformedURLException e) {
-			Date today = new Date();
-			String log_content = "\n" + hsr.getHeader("x-forwarded-for") + "	"
-					+ hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri
-					+ "	" + "failed";
-			
-			logger.info(log_content);
-			e.printStackTrace();
-			
-			return Response
-					.ok(new ErrorStreamingOutput("Malformed REST CALL!!")).type(MediaType.TEXT_XML).build();
-			//return new ErrorStreamingOutput("Malformed REST CALL!!");
-		}
-		
-		HttpURLConnection conn = null;
-		try {
-			conn = (HttpURLConnection) newURL.openConnection();
-		} catch (IOException e) {
-			Date today = new Date();
-			String log_content = "\n" + hsr.getHeader("x-forwarded-for") + "	"
-					+ hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri
-					+ "	" + "failed";
-			e.printStackTrace();
-			return Response
-					.ok(new ErrorStreamingOutput("COULD NOT OPEN COLLECTION to " + newURL)).type(MediaType.TEXT_XML).build();
-			//return new ErrorStreamingOutput("COULD NOT OPEN COLLECTION to " + newURL);
-		}
+		debugLogger.info("dispatch to " + host);
+		URI newURI = uriBuilder.host(host).port(Integer.valueOf(ParamDef.getConfig().getProperty("solr.ocr.port")))
+				.replacePath("solr/" + ParamDef.getConfig().getProperty("solr.ocr.core") + "/select").build();
+		debugLogger.info("new OCR uri: " + newURI);
 
 		try {
+			URL newURL = newURI.toURL();
+			HttpURLConnection conn = (HttpURLConnection) newURL.openConnection();
 			int responseCode = conn.getResponseCode();
 			if (responseCode != 200) {
 				Date today = new Date();
-				String log_content = "\n" + hsr.getHeader("x-forwarded-for") + "	"
-						+ hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri
-						+ "	" + "failed";
-				
-				logger.info(log_content);
-				
-				return Response
-						.ok(new ErrorStreamingOutput("RESPONSE CODE: " + responseCode)).type(MediaType.TEXT_XML).build();
-			//	return new ErrorStreamingOutput("RESPONSE CODE: " + responseCode);
-			//	throw new IOException(conn.getResponseMessage());
+				String logContent = "\n" + hsr.getHeader("x-forwarded-for") + "	" + hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri + "	" + "failed";
+				auditLogger.info(logContent);
+				debugLogger.warn("response code: " + responseCode);
+				return Response.ok(new ErrorStreamingOutput("RESPONSE CODE: " + responseCode)).type(MediaType.TEXT_XML).build();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			
+		
+			StreamingOutput returnStream = new CommonQueryStreamingOutput(conn);
+			// get content type from solr response and set content type in response
+			// with this value
+			String contentType = conn.getContentType();
 			Date today = new Date();
-			String log_content = "\n" + hsr.getHeader("x-forwarded-for") + "	"
-					+ hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri
-					+ "	" + "failed";
-			
-			logger.info(log_content);
-			
-			return Response
-					.ok(new ErrorStreamingOutput("ERROR OCCURED CONNECTING TO SOLR  SERVER!!")).type(MediaType.TEXT_XML).build();
-			
-			//return new ErrorStreamingOutput("ERROR OCCURED CONNECTING TO SOLR  SERVER!!");
-		}
-		
-		StreamingOutput returnStream = new CommonQueryStreamingOutput(conn);
-		// get content type from solr response and set content type in response with this value
-		String contentType = conn.getContentType();
-		
-		Date today = new Date();
+			String logContent = "\n" + hsr.getHeader("x-forwarded-for") + "	" + hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri + "	" + "allowed";
+			auditLogger.info(logContent);
 
-		String log_content = "\n" + hsr.getHeader("x-forwarded-for")
-				+ "	" + hsr.getRemoteAddr() + "	" + today.toString()
-				+ "	" + uri + "	" + "allowed";
-		
-		logger.info(log_content);
-		
-		return Response
-				.ok(returnStream).type(contentType).build();
-	//	return returnStream;		
+			return Response.ok(returnStream).type(contentType).build();
+		} catch (MalformedURLException e) {
+			Date today = new Date();
+			String logContent = "\n" + hsr.getHeader("x-forwarded-for") + "	" + hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri + "	" + "failed";
+			auditLogger.info(logContent);
+			debugLogger.debug("MalformedURLException: ", e);
+			return Response.ok(new ErrorStreamingOutput("Malformed REST CALL!!")).type(MediaType.TEXT_XML).build();
+		}  catch (IOException e) {
+			Date today = new Date();
+			String logContent = "\n" + hsr.getHeader("x-forwarded-for") + "	" + hsr.getRemoteAddr() + "	" + today.toString() + "	" + uri + "	" + "failed";
+			auditLogger.info(logContent);
+			debugLogger.debug("IOException: ", e);
+			return Response.ok(new ErrorStreamingOutput("ERROR OCCURED CONNECTING TO SOLR  SERVER!!")).type(MediaType.TEXT_XML).build();
+		}
 	}
 }
